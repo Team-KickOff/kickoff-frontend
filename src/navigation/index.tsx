278c921d68f2c314394d14ad5617d568d88f5d75
Kopite22 <nulee1000@gmail.com>
2024-01-14 15:47:13 +0900

    init project

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import FirstScreen from "../screens/appScreens";
+
+const Stack = createStackNavigator();
 
 const Navigation = () => {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={"app"} component={FirstScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
+
+export default Navigation;
