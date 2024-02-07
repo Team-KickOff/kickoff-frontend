5b4972431db09980a3047bc615728978b5d07f66
Kopite22 <nulee1000@gmail.com>
2024-02-07 17:45:58 +0900

    [천누리][create basic theme]

---
+++ src/shared/theme/theme.ts
@@ -0,0 +1,18 @@
+export const themes = {
+  color: {
+    primary: '#1A2C56',
+    white: '#FFFFFF',
+    black: '#000000',
+    transparentBlack: 'rgb(0, 0, 0, 0.5)',
+    secondary: '#9c9c9c',
+  },
+  fontSize: {
+    xsmall: 10,
+    small: 12,
+    medium: 14,
+    large: 16,
+    xlarge: 18,
+    xxlarge: 20,
+  },
+  fontWeight: { bold: 700, regular: 400 },
+};
